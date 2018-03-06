eae12a3f010ca14b35ff9195814a30317d2c7d0a
Sam Chung <samchung@gmail.com>
2018-03-05 18:26:13 -0800

    almost done

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,5 +13,53 @@ Your task is to create a Python script that analyzes the records to calculate ea
 - The greatest decrease in revenue (date and amount) over the entire period
 """
 
+import os
+import glob
+import csv
 
+def list_csv():
+    """ Create a list of CSV files in the current directory"""
+    for file in (glob.glob('*.csv')): 
+        with open(file, 'r') as f:
+            content = csv.reader(f, delimiter=',')
+            # Skip the header row
+            next(content)
+            content_list = list(content)
+
+            total_months = len(content_list)
+            total_revenue = 0
+            
+            greatest_revenue_decrease = 0
+            greatest_revenue_decrease_month = ""
+
+            greatest_revenue_increase = 0
+            greatest_revenue_increase_month = ""
+
+            for item in content_list:
+                month = item[0]
+                revenue = int(item[1])
+                total_revenue += revenue
+
+                if revenue < greatest_revenue_decrease:
+                    greatest_revenue_decrease = revenue
+                    greatest_revenue_decrease_month = month
+                
+                elif revenue > greatest_revenue_increase:
+                        greatest_revenue_increase = revenue
+                        greatest_revenue_increase_month = month
+
+            final_output = "Source Data File: " + file +"\n\n" \
+                        + "Financial Analysis\n----------------------------\n" \
+                        + "Total Months: """ +  str(total_months) + "\n" \
+                        + "Average Revenue Change: " + str(int(total_revenue / total_months)) + "\n" \
+                        + "Greatest Increase in Revenue: " + greatest_revenue_increase_month + " (" + str(greatest_revenue_increase) + ")" + "\n"\
+                        + "Greatest Decrease in Revenue: " + greatest_revenue_decrease_month + " (" + str(greatest_revenue_decrease) + ")\n\n"
+
+
+            print(final_output)
+            with open(file[:-4]+".txt", 'w+') as f:
+                f.write(final_output)
+
+if __name__ == "__main__":
+    list_csv()
 
