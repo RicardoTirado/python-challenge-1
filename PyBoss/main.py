b838a42cf1ff1b967eed421324d69d77c76f081a
Sam Chung <samchung@gmail.com>
2018-03-06 19:19:25 -0800

    Start

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,28 +1,19 @@
 """
-In this challenge, you are tasked with creating a Python script for analyzing the financial records of your company. 
-You will be given two sets of revenue data (budget_data_1.csv and budget_data_2.csv). 
-Each dataset is composed of two columns: Date and Revenue. (Thankfully, your company has rather lax standards for 
-accounting so the records are simple.)
+In this challenge, you get to be the boss. You oversee hundreds of employees across the country developing Tuna 2.0, a 
+world-changing snack food based on canned tuna fish. Alas, being the boss isn't all fun, games, and self-adulation. 
+The company recently decided to purchase a new HR system, and unfortunately for you, the new system requires employee 
+records be stored completely differently.
 
-Your task is to create a Python script that analyzes the records to calculate each of the following:
+Your task is to help bridge the gap by creating a Python script able to convert your employee records to the required format. 
 
-- The total number of months included in the dataset
-- The total amount of revenue gained over the entire period
-- The average change in revenue between months over the entire period
-- The greatest increase in revenue (date and amount) over the entire period
-- The greatest decrease in revenue (date and amount) over the entire period
+The required conversions are as follows:
+
+- The Name column should be split into separate First Name and Last Name columns.
+- The DOB data should be re-written into DD/MM/YYYY format.
+- The SSN data should be re-written such that the first five numbers are hidden from view.
+- The State data should be re-written as simple two-letter abbreviations.
 """
 
 import os
 import csv
-
-csv_files = [os.path.join('raw_data', 'election_data_1.csv'), os.path.join('raw_data', 'election_data_2.csv')]
-for file in csv_files:
-    print(file)
-    with open(file, newline='') as cvsfile:
-        csvreader = csv.reader(cvsfile, delimiter =',')
-        print(csvreader)
-# def list_csv():
-# if __name__ == "__main__":
-#     list_csv()
 
